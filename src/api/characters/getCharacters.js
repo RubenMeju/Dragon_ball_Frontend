15f25a72b1fb04c388cb296317dbb636e2538b7c
Rubén Y <rubenmeju@outlook.es>
2023-09-17 22:50:06 +0200

    eslint & env

--- src/api/characters/getCharacters.js
+++ src/api/characters/getCharacters.js
@@ -1,13 +1,11 @@
 export async function getCharacters(offset = '') {
-  const res = await fetch(
-    `http://127.0.0.1:8000/characters/?limit=15&offset=${offset}`,
-    {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json'
-      }
+  const URL_BASE = process.env.URL_BASE
+  const res = await fetch(`${URL_BASE}/characters/?limit=15&offset=${offset}`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
     }
-  )
+  })
   if (!res.ok) {
     console.log('error:', res.status)
     return
