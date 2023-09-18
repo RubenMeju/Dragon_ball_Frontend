ae3aa5280e364d887b022b793a52987ed3315747
Rubén Y <rubenmeju@outlook.es>
2023-09-18 12:10:42 +0200

    fix enviroment variable

--- src/api/characters/getCharacters.js
+++ src/api/characters/getCharacters.js
@@ -1,17 +1,19 @@
 export async function getCharacters(offset = '') {
-  const URL_BASE = process.env.URL_BASE
-  const res = await fetch(`${URL_BASE}/characters/?limit=15&offset=${offset}`, {
-    method: 'GET',
-    headers: {
-      'content-type': 'application/json'
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL_BASE}/characters/?limit=15&offset=${offset}`,
+    {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
     }
-  })
+  )
   if (!res.ok) {
     console.log('error:', res.status)
     return
   }
   const data = await res.json()
-  console.log('respuestaaa:', data)
+  // console.log('respuestaaa:', data)
 
   return data
 }
